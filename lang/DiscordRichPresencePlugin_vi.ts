7f49bef71edcefc9672f3910cffad91a1139eaee
Trần Nam Tuấn <tuantran1632001@gmail.com>
2025-03-16 07:27:43 +0700

    Update VI translation

--- lang/DiscordRichPresencePlugin_vi.ts
+++ lang/DiscordRichPresencePlugin_vi.ts
@@ -4,41 +4,46 @@
 <context>
     <name>QtC::DiscordRichPresence</name>
     <message>
-        <location filename="../src/plugin.cpp" line="36"/>
+        <location filename="../src/plugin.cpp" line="60"/>
         <source>Reload Discord Rich Presence</source>
         <translation>Tải lại Discord Rich Presence</translation>
     </message>
     <message>
-        <location filename="../src/plugin.cpp" line="70"/>
-        <source>Cannot create Discord Game SDK instance. Error %1.</source>
-        <translation>Không thể tạo đối tượng Discord Game SDK. Lỗi %1.</translation>
+        <location filename="../src/plugin.cpp" line="117"/>
+        <source>Cannot connect to Discord Client. Error %1. Is Discord Running?</source>
+        <translation>Không thể kết nối với Discord Client. Lỗi %1. Discord có đang chạy không?</translation>
     </message>
     <message>
-        <location filename="../src/plugin.cpp" line="97"/>
+        <location filename="../src/plugin.cpp" line="121"/>
+        <source>Cannot connect to Discord Client. Error %1.</source>
+        <translation>Không thể kết nối với Discord Client. Lỗi %1.</translation>
+    </message>
+    <message>
+        <location filename="../src/plugin.cpp" line="149"/>
         <source>Discord Rich Presence connected.</source>
         <translation>Đã kết nối Discord Rich Presence.</translation>
     </message>
     <message>
-        <location filename="../src/plugin.cpp" line="130"/>
+        <location filename="../src/plugin.cpp" line="182"/>
         <source>%1&apos;s UI: %2</source>
         <comment>&apos;%1&apos; is project name.</comment>
         <translatorcomment>&apos;%1&apos; là tên dự án.</translatorcomment>
         <translation>UI của %1: %2</translation>
     </message>
     <message>
-        <location filename="../src/plugin.cpp" line="133"/>
+        <location filename="../src/plugin.cpp" line="185"/>
         <source>%1&apos;s file: %2</source>
         <comment>&apos;%1&apos; is project name.</comment>
         <translatorcomment>&apos;%1&apos; là tên dự án.</translatorcomment>
         <translation>File của %1: %2</translation>
     </message>
     <message>
-        <location filename="../src/plugin.cpp" line="146"/>
+        <location filename="../src/plugin.cpp" line="198"/>
         <source>Project %1</source>
         <translation>Dự án.%1</translation>
     </message>
     <message>
-        <location filename="../src/plugin.cpp" line="160"/>
+        <location filename="../src/plugin.cpp" line="213"/>
         <source>Cannot update Discord Activity. Error %1.</source>
         <translation>Không thể cập nhật Hoạt động Discord. Lỗi %1.</translation>
     </message>
